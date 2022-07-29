5c8a60fd035fa90b0df29cba6767625afe218ccc
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-07-29 14:40:14 -0300

    carrinho-estilo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
     mensagemAlerta: "Item Adicionado",
     alertaAtivo: false,
   },
@@ -52,6 +53,11 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) {
         this.produto = false;
+      }
+    },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.carrinhoAtivo = false;
       }
     },
     adicionarItem() {
